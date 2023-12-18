2f0831f7a0649e9f47c7785e4d151ca47e828ca9
Renata Mansilla <renata.mansilla@uala.com.ar>
2023-12-18 19:16:05 -0300

    add list, add create

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"go-intro-validation/model"
+	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
 var reader *bufio.Reader
+var shapes = make(map[string]model.Shape)
 
 func main() {
 	var option string
@@ -16,14 +20,14 @@ func main() {
 	for {
 		fmt.Println("<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>")
 		fmt.Println("Seleccione la acción a relizar:")
-		fmt.Println("a. listar figuras")
-		fmt.Println("b. obtener figura por su ID")
-		fmt.Println("c. agregar nueva figura")
+		fmt.Println("a. Listar figuras")
+		fmt.Println("b. Obtener figura por su ID")
+		fmt.Println("c. Agregar nueva figura")
+		fmt.Println("Presione \"q\" para salir.")
 		fmt.Println("<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>")
 
 		option = readLine()
-
-		if option == "quit" || option == "q" {
+		if quit(option) {
 			break
 		}
 
@@ -36,9 +40,11 @@ func main() {
 			createShape()
 		}
 	}
-
 	fmt.Println("Byee")
+}
 
+func quit(option string) bool {
+	return option == "quit" || option == "q"
 }
 
 func readLine() string {
@@ -50,15 +56,93 @@ func readLine() string {
 }
 
 func createShape() {
-	fmt.Println("seleccione el tipo de figura:")
-	fmt.Println("ingrese valor a:")
-	fmt.Println("ingrese valor b:")
+	for {
+		fmt.Println("Seleccione el tipo de figura, o presione \"q\" para volver:")
+		fmt.Println("a. Elipse")
+		fmt.Println("b. Rectángulo")
+		fmt.Println("c. Triángulo")
+
+		var option string = readLine()
+		if quit(option) {
+			return
+		}
+
+		switch option {
+		case "a", "elipse":
+			var radiusA, radiusB string
+			fmt.Println("ingrese radio a:")
+			radiusA = readLine()
+			if quit(radiusA) {
+				return
+			}
+
+			fmt.Println("ingrese radio b:")
+			radiusB = readLine()
+			if quit(radiusB) {
+				return
+			}
+
+			ellipse := model.CreateEllipse(toFloat(radiusA), toFloat(radiusB))
+			ellipse.Print()
+			shapes[ellipse.Id.String()] = ellipse
+		case "b", "rectangulo":
+			var height, width string
+			fmt.Println("ingrese alto:")
+			height = readLine()
+			if quit(height) {
+				return
+			}
+
+			fmt.Println("ingrese ancho:")
+			width = readLine()
+			if quit(width) {
+				return
+			}
+
+			rectangle := model.CreateRectangle(toFloat(height), toFloat(width))
+			rectangle.Print()
+			shapes[rectangle.Id.String()] = rectangle
+		case "c", "triangulo":
+			var base, height string
+			fmt.Println("ingrese base:")
+			base = readLine()
+			if quit(base) {
+				return
+			}
+
+			fmt.Println("ingrese altura:")
+			height = readLine()
+			if quit(height) {
+				return
+			}
+
+			triangle := model.CreateTriangle(toFloat(base), toFloat(height))
+			triangle.Print()
+			shapes[triangle.Id.String()] = triangle
+		}
+	}
+
+}
+
+func toFloat(str string) float64 {
+	radiusAfloat, err := strconv.ParseFloat(str, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return radiusAfloat
 }
 
 func getShapeById() {
-	fmt.Println("ingrese ID:")
+	fmt.Println("ingrese Id:")
 }
 
 func listShapes() {
-	fmt.Println("listado de figuras")
+	if len(shapes) == 0 {
+		fmt.Println("La lista está vacía. Agregue figuras y vuelva a intentar.")
+		return
+	} else {
+		for _, shape := range shapes {
+			shape.Print()
+		}
+	}
 }
