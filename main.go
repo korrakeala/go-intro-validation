c0573c767e536a6072d91da0aff1d871bf2cbeae
Renata Mansilla <renata.mansilla@uala.com.ar>
2023-12-19 17:23:41 -0300

    add get by id, refactor

--- main.go
+++ main.go
@@ -14,19 +14,12 @@ var reader *bufio.Reader
 var shapes = make(map[string]model.Shape)
 
 func main() {
-	var option string
 	reader = bufio.NewReader(os.Stdin)
 
 	for {
-		fmt.Println("<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>")
-		fmt.Println("Seleccione la acción a relizar:")
-		fmt.Println("a. Listar figuras")
-		fmt.Println("b. Obtener figura por su ID")
-		fmt.Println("c. Agregar nueva figura")
-		fmt.Println("Presione \"q\" para salir.")
-		fmt.Println("<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>")
+		printMenu()
+		option := readLine()
 
-		option = readLine()
 		if quit(option) {
 			break
 		}
@@ -38,9 +31,21 @@ func main() {
 			getShapeById()
 		case "c", "agregar":
 			createShape()
+		default:
+			fmt.Println("Opción no válida. Intente de nuevo.")
 		}
 	}
 	fmt.Println("Byee")
+}
+
+func printMenu() {
+	fmt.Println("<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>")
+	fmt.Println("Seleccione la acción a relizar:")
+	fmt.Println("a. Listar figuras")
+	fmt.Println("b. Obtener figura por su ID")
+	fmt.Println("c. Agregar nueva figura")
+	fmt.Println("Presione \"q\" para salir.")
+	fmt.Println("<<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>><<>>")
 }
 
 func quit(option string) bool {
@@ -57,92 +62,117 @@ func readLine() string {
 
 func createShape() {
 	for {
-		fmt.Println("Seleccione el tipo de figura, o presione \"q\" para volver:")
-		fmt.Println("a. Elipse")
-		fmt.Println("b. Rectángulo")
-		fmt.Println("c. Triángulo")
+		printShapeMenu()
+		option := readLine()
 
-		var option string = readLine()
 		if quit(option) {
 			return
 		}
 
-		switch option {
-		case "a", "elipse":
-			var radiusA, radiusB string
-			fmt.Println("ingrese radio a:")
-			radiusA = readLine()
-			if quit(radiusA) {
-				return
-			}
-
-			fmt.Println("ingrese radio b:")
-			radiusB = readLine()
-			if quit(radiusB) {
-				return
-			}
-
-			ellipse := model.CreateEllipse(toFloat(radiusA), toFloat(radiusB))
-			ellipse.Print()
-			shapes[ellipse.Id.String()] = ellipse
-		case "b", "rectangulo":
-			var height, width string
-			fmt.Println("ingrese alto:")
-			height = readLine()
-			if quit(height) {
-				return
-			}
-
-			fmt.Println("ingrese ancho:")
-			width = readLine()
-			if quit(width) {
-				return
-			}
-
-			rectangle := model.CreateRectangle(toFloat(height), toFloat(width))
-			rectangle.Print()
-			shapes[rectangle.Id.String()] = rectangle
-		case "c", "triangulo":
-			var base, height string
-			fmt.Println("ingrese base:")
-			base = readLine()
-			if quit(base) {
-				return
-			}
-
-			fmt.Println("ingrese altura:")
-			height = readLine()
-			if quit(height) {
-				return
-			}
-
-			triangle := model.CreateTriangle(toFloat(base), toFloat(height))
-			triangle.Print()
-			shapes[triangle.Id.String()] = triangle
+		shapeType, ok := shapeTypeMap[option]
+		if !ok {
+			fmt.Println("Opción no válida. Intente de nuevo.")
+			continue
 		}
+
+		dimensions := getShapeDimensions(shapeType)
+		if dimensions == nil {
+			return
+		}
+
+		shape := createAndAddShape(shapeType, dimensions)
+		shape.Print()
+
 	}
 
+}
+
+func printShapeMenu() {
+	fmt.Println("Seleccione el tipo de figura, o presione \"q\" para volver:")
+	fmt.Println("a. Elipse")
+	fmt.Println("b. Rectángulo")
+	fmt.Println("c. Triángulo")
+}
+
+func getShapeDimensions(shapeType string) []string {
+	var dimensions []string
+
+	for _, dimension := range shapeDimensions[shapeType] {
+		fmt.Printf("Ingrese %s:", dimension)
+		value := readLine()
+
+		if quit(value) {
+			return nil
+		}
+
+		dimensions = append(dimensions, value)
+	}
+
+	return dimensions
+}
+
+func createAndAddShape(shapeType string, dimensions []string) model.Shape {
+	switch shapeType {
+	case "elipse":
+		radiusA, radiusB := dimensions[0], dimensions[1]
+		ellipse := model.CreateEllipse(toFloat(radiusA), toFloat(radiusB))
+		shapes[ellipse.Id.String()] = ellipse
+		return ellipse
+	case "rectangulo":
+		height, width := dimensions[0], dimensions[1]
+		rectangle := model.CreateRectangle(toFloat(height), toFloat(width))
+		shapes[rectangle.Id.String()] = rectangle
+		return rectangle
+	case "triangulo":
+		base, height := dimensions[0], dimensions[1]
+		triangle := model.CreateTriangle(toFloat(base), toFloat(height))
+		shapes[triangle.Id.String()] = triangle
+		return triangle
+	default:
+		return nil
+	}
 }
 
 func toFloat(str string) float64 {
-	radiusAfloat, err := strconv.ParseFloat(str, 64)
+	val, err := strconv.ParseFloat(str, 64)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error al convertir la entrada a número:", err)
 	}
-	return radiusAfloat
+	return val
 }
 
 func getShapeById() {
-	fmt.Println("ingrese Id:")
+	fmt.Println("Ingrese Id:")
+	id := readLine()
+
+	shape, exists := shapes[id]
+	if !exists {
+		fmt.Println("No se encontró una figura con ese Id.")
+		return
+	}
+
+	shape.Print()
 }
 
 func listShapes() {
 	if len(shapes) == 0 {
 		fmt.Println("La lista está vacía. Agregue figuras y vuelva a intentar.")
 		return
-	} else {
-		for _, shape := range shapes {
-			shape.Print()
-		}
 	}
+
+	for _, shape := range shapes {
+		shape.Print()
+	}
+}
+
+var shapeTypeMap = map[string]string{
+	"a": "elipse",
+	"b": "rectangulo",
+	"c": "triangulo",
+}
+
+var shapeDimensions = map[string][]string{
+	"elipse":     {"radio A", "radio B"},
+	"rectangulo": {"alto", "ancho"},
+	"triangulo":  {"base", "altura"},
 }
